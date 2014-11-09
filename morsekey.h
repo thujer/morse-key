f716cced66b8c771e6f2c927e3c313a5ae217fa4
Tomas Hujer <thujer@gmail.com>
2014-11-09 17:21:11 +0100

    First commit

---
+++ morsekey.h
@@ -0,0 +1,7 @@
+
+
+extern bit         bMKey_Code_ACCEPTED;  // Kod akceptovan, nuluje se rucne nebo s kazdym dalsim znakem
+extern bit         bMKey_Emergency;      // Flag akceptace nouzoveho kodu
+extern void         MKey_Init();
+extern void         MKey_Proc();
+
